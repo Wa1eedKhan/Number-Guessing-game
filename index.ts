57796214f8d28b45e969edbda8abbe613e69f994
Waleed khan <166182415+Waleed0248@users.noreply.github.com>
2024-09-03 22:24:54 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import { log } from "console";
+import inquirer from "inquirer"
+import chalk from "chalk"
+console.log(chalk.bold.yellow("=".repeat(70)));
+console.log(chalk.yellow("\n \t        Welcome to our Guessing Game     \n"))
+console.log(chalk.bold.yellow("=".repeat(70)));
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answers = await inquirer.prompt([
+{
+name : "userGuessedNumber",
+type : "number",
+message : chalk.blue.bold("Please guess a number between 1 to 10")
+},
+]);
+
+if(answers.userGuessedNumber === randomNumber){
+    console.log(chalk.green("Congratulations!, You have guessed a right number"))
+}
+else{
+    console.log(chalk.red("You have guessed a wrong number"));
+}
